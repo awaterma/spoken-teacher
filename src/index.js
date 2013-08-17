632015cee962ed44500aa9f11505ca545865f126
Andrew Waterman <awaterma@ecosur.mx>
2013-08-16 19:30:49 -0500

    Updates for presentation.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 var blessed = require('blessed');
 var say = require('say');
-var Q = require('q');
 var fs = require('fs');
-var through = require('through');
 
-var englishVoice = 'Alex';
+var englishVoice = 'Samantha';
 var spanishVoice = 'Paulina';
+var tseltalVoice = 'Paulina';
 
 /* Load the phrases file into memory */
 var db = JSON.parse(fs.readFileSync('src/resources/phrases.json', 'utf8'));
@@ -14,39 +13,34 @@ var counter = 0;
 var screen = blessed.screen();
 screen.smartCSR = true;
 
-var bienvenidos = "Bienvenidos a su maestra virtual de Íngles";
-var welcome = "Welcome to virtual English teacher";
-
 var topbox = blessed.box({
-  top: '0',
+  top: '33%',
   left: 'center',
   width: '80%',
-  height: '50%',
+  height: '33%',
   border: {
     type: 'line',
     fg: '#ffffff'
   },
   fg: 'red',
   bg: 'yellow',
-  content: '{center}' + bienvenidos,
   tags: true,
   hoverEffects: {
     bg: 'green'
   }
 });
 
-var bottombox = blessed.box({  
-  top: '50%',
+var midbox = blessed.box({
+  top: '66%',
   left: 'center',
   width: '80%',
-  height: '50%',
+  height: '33%',
   border: {
     type: 'line',
     fg: '#ffffff'
   },
   fg: 'white',
-  bg: 'red',
-  content: '{center}' + welcome, 
+  bg: 'black',
   tags: true,
   hoverEffects: {
     bg: 'blue'
@@ -54,8 +48,23 @@ var bottombox = blessed.box({
 
 })
 
-topbox.phrase = bienvenidos;
-bottombox.phrase = welcome;
+var bottombox = blessed.box({  
+  top: '0',
+  left: 'center',
+  width: '80%',
+  height: '33%',
+  border: {
+    type: 'line',
+    fg: '#ffffff'
+  },
+  fg: 'white',
+  bg: 'red',
+  tags: true,
+  hoverEffects: {
+    bg: 'blue'
+  }
+
+})
 
 function switchContent(c) {
   if (c > db.phrases.length - 1 || c < 0) {
@@ -65,12 +74,18 @@ function switchContent(c) {
   topbox.phrase = (db.phrases[c].spanish);
   bottombox.setContent('{center}' + db.phrases[c].english);
   bottombox.phrase = db.phrases[c].english;
+  midbox.setContent('{center}' + db.phrases[c].tseltal);
+  midbox.phrase = db.phrases[c].tseltal;
   screen.render();
 }
+
+/* Initialize conent */
+switchContent(0);
 
 // Append our box to the screen.
 screen.append(topbox);
 screen.append(bottombox);
+screen.append(midbox);
 
 // If our box is clicked, change the content.
 topbox.on('click', function(data) {
@@ -79,6 +94,10 @@ topbox.on('click', function(data) {
 
 bottombox.on('click', function(data) {
   say.speak(englishVoice, bottombox.phrase);
+});
+
+midbox.on('click', function(data) {
+  say.speak(tseltalVoice, midbox.phrase);
 });
 
 screen.key(['space','right','return'], function(ch, key) {
